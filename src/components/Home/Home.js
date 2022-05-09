c3475ba76a8e4ef0c06d443225c359a954949667
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-05-10 03:18:27 +0600

    banner done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import InventoryItem from '../InventoryItem/InventoryItem';
 
 const Home = () => {
           return (
                     <div>
                     
 
-                        <Banner></Banner>   
+                        <Banner></Banner>
+                        <InventoryItem></InventoryItem>  
+
+
                     </div>
           );
 };
---
+++ src/components/InventoryItem/InventoryItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InventoryItem = () => {
+          return (
+                    <div>
+                       <h3 className='text-center'>Inventory Item</h3> 
+                    </div>
+          );
+};
+
+export default InventoryItem;
